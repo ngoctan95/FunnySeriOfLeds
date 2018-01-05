3865decde0f09b20e70f83cf2b18bb682c3808ab
ngoctan95 <nguyenngoctan44@gmail.com>
2018-01-06 00:18:23 +0700

    iNIT Leds

--- FunnySeriOfLeds/ViewController.swift
+++ FunnySeriOfLeds/ViewController.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let image = UIImage(named : "Balloon")
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for oldIndex in 0..<10{
+            for index in 0..<10{
+                let img = UIImageView(image: image)
+                img.center = CGPoint(x: CGFloat(index+1) * (getMarginDevice(count: 10)), y: CGFloat(oldIndex+1)*getMarginDevice(count: 10))
+                view.addSubview(img)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getMarginDevice(count : Int) -> CGFloat{
+       // print("\(((self.view.bounds.size.width - (CGFloat(count) * (image?.size.width)!))/(CGFloat(count+1))))")
+        return ((self.view.bounds.size.width - 100)/(CGFloat(count-1)))
     }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
